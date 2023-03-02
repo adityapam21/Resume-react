9d6047304cad0890313a6d0cc31a59d227b9dfde
adityapam21 <124274431+adityapam21@users.noreply.github.com>
2023-03-02 09:30:35 +0700

    initial commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Aditya Pamungkas </span>
+            from <span className="purple"> Jakarta, Indonesia.</span>
+            <br />I am a student in Full stack Web Development and I am working as Content Quality in TikTok Indonesia.
+            <br />
+            <br />
+            Apart from my routine, this is another activity that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Basketball
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
